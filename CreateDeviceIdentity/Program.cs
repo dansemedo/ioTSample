c30d6dfc0795d0c82214184d5f2a691bd6e35381
Daniel Semedo <danpin@microsoft.com>
2017-12-14 17:31:33 -0200

    files

---
+++ CreateDeviceIdentity/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=hubsemedo.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=i7pfC3t1yRZ5QvKxrjliRapbbCHf0RUivNnhQs+cNbQ=";
+        static void Main(string[] args)
+        {
+
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
---
+++ SimulatedDevice/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+
+        static DeviceClient deviceClient;
+        static string iotHubUri = "hubsemedo.azure-devices.net";
+        static string deviceKey = "TNERQVL//o7DEadQDhXnU/Tl8Ljjyjc1iOi9aeuOb9w=";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), TransportType.Mqtt);
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+           // double avgWindSpeed = 10; // m/s
+          //  Random rand = new Random();
+
+            while (true)
+            {
+                //double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                using (var fs = File.OpenRead(@"C:\output.csv"))
+                using (var reader = new StreamReader(fs))
+                {
+                   // var coord0 = new List<strin();
+                    //var coord1 = new List<string>();
+
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        var coord0 = (values[0]);
+                        var coord1 = (values[1]);
+                        var ratecsv = (values[4]);
+                        var colorcsv = (values[6]);
+
+
+
+                        //variavel para ser consumida no ioThub
+
+                        var telemetryDataPoint = new
+                        {
+                            deviceId = "myFirstDevice",
+                            latitude = coord0,
+                            longitude = coord1,
+                            rate = ratecsv,
+                            color = colorcsv
+
+                    };
+                        var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                        var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                        await deviceClient.SendEventAsync(message);
+                        Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                       // await Task.Delay(1);
+
+                    }
+                }
+            }
+        }
+    }
+}
